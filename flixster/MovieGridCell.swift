ef50ee7b4de0e56b1a4d6c98054feefabd7ac547
Jennifer Sophia <jennifersophia1411@gmail.com>
2022-02-15 23:51:19 -0800

    Flix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jennifer Lopez on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -36,7 +36,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                                                 
                  
                  self.tableView.reloadData()
-                    print(dataDictionary)
                     // TODO: Get the array of movies
                     
                     // TODO: Store the movies in a property to use elsewhere
@@ -70,5 +69,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        print("Loading up the details screen")
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MoviesDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
